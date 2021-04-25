8c07259c424fe1fa74b1691f9c0a9295440bb644
Knoa <mike1124@naver.com>
2021-04-26 00:35:25 +0900

    🎨 Complete typeChain course

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "noah",
-    age: 22,
-    gender: "male"
+const CryptoJs = require("crypto-js");
+// Class 의 constructor 사용 시 interface 사용 안해도 된다.
+// interface Block {
+//     index: number;
+//     hash: string;
+//     previousHash: string;
+//     data: string;
+//     timestamp: number;
+// }
+class Block {
+    // 생성자 함수로 클래스 new 로 할당시 바로 호출되는 함수 ( 초기화 함수? )
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+// static 사용 시 Block 의 인스턴스 생성 후 변수 할당하여 하지 않고 바로 사용 가능하다.
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+// genesisBlock 이 class Block 이여야 한다.
+const genesisBlock = new Block(0, "2309423", "", "Hello", 123456);
+// blockchain 이 class Block 의 array 형태여야 한다.
+let blockchain = [genesisBlock];
+// 함수 표현식으로 정의된 getBlockChain 의 경우, Block[] Block array 형태를 리턴한다.
+const getBlockChain = () => blockchain;
+// 마찬가지지만, Block 형태로 리턴한다.
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+// timeStamp 의 경우, Math.round(new Date().getTime() / 1000); => 숫자 형태로 리턴한다.
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+// createNewBlock => data: string 으로 파라미터를 받아서 => Block 형태로 리턴한다.
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock(); //최근 저장된 블록을 가져온다.
+    const newIndex = previousBlock.index + 1; //블록의 인덱스에 + 1 을 newIndex 에 저장한다.
+    const newTimeStamp = getNewTimeStamp(); //TimeStamp number 값으로 저장한다.
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data); //calculateBlockHash => Crypto SHA256으로 String 생성 후 newHash 에 저장 한다.
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp); //새 블록을 만든다. ( 새로운 파라미터 정보를 넘겨준다. )
+    addBlock(newBlock);
+    return newBlock; // 새 블록을 리턴한다.
 };
-const name = "Noah", age = 24, gender = "male";
-const sayHi = (person) => {
-    return `Hello ${person.name}, you ar ${person.age}, you are a ${person.gender}.`;
+const getHashForBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
-console.log(sayHi(person));
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,23 +1,104 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+import * as CryptoJs from 'crypto-js';
+
+// Class 의 constructor 사용 시 interface 사용 안해도 된다.
+
+// interface Block {
+//     index: number;
+//     hash: string;
+//     previousHash: string;
+//     data: string;
+//     timestamp: number;
+// }
+
+class Block {
+    // static 사용 시 Block 의 인스턴스 생성 후 변수 할당하여 하지 않고 바로 사용 가능하다.
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string =>
+        CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    // 생성자 함수로 클래스 new 로 할당시 바로 호출되는 함수 ( 초기화 함수? )
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number,
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name: "noah",
-    age: 22,
-    gender: "male"
+// genesisBlock 이 class Block 이여야 한다.
+const genesisBlock: Block = new Block(0, "2309423", "", "Hello", 123456);
+// blockchain 이 class Block 의 array 형태여야 한다.
+let blockchain: Block[] = [genesisBlock];
+// 함수 표현식으로 정의된 getBlockChain 의 경우, Block[] Block array 형태를 리턴한다.
+const getBlockChain = (): Block[] => blockchain;
+// 마찬가지지만, Block 형태로 리턴한다.
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+// timeStamp 의 경우, Math.round(new Date().getTime() / 1000); => 숫자 형태로 리턴한다.
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+// createNewBlock => data: string 으로 파라미터를 받아서 => Block 형태로 리턴한다.
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock(); //최근 저장된 블록을 가져온다.
+    const newIndex: number = previousBlock.index + 1; //블록의 인덱스에 + 1 을 newIndex 에 저장한다.
+    const newTimeStamp: number = getNewTimeStamp(); //TimeStamp number 값으로 저장한다.
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data); //calculateBlockHash => Crypto SHA256으로 String 생성 후 newHash 에 저장 한다.
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp); //새 블록을 만든다. ( 새로운 파라미터 정보를 넘겨준다. )
+    addBlock(newBlock);
+    return newBlock; // 새 블록을 리턴한다.
 }
 
-const name = "Noah",
-    age = 24,
-    gender = "male";
+const getHashForBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 
-const sayHi = (person: Human) => {
-    return `Hello ${person.name}, you ar ${person.age}, you are a ${person.gender}.`;
-}
+const isBlockValid = (
+    candidateBlock: Block,
+    previousBlock: Block
+): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+};
 
-console.log(sayHi(person));
+const addBlock = (candidateBlock: Block): void => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+
+console.log(blockchain);
 
 export { };
