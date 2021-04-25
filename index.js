3f47a6ff98c648119fa644457c51978a8270e005
Knoa <mike1124@naver.com>
2021-04-25 17:50:48 +0900

    🎨 Use interface define function parameter object

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "noah",
+    age: 22,
+    gender: "male"
+};
+const name = "Noah", age = 24, gender = "male";
+const sayHi = (person) => {
+    return `Hello ${person.name}, you ar ${person.age}, you are a ${person.gender}.`;
+};
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "noah",
+    age: 22,
+    gender: "male"
+}
+
+const name = "Noah",
+    age = 24,
+    gender = "male";
+
+const sayHi = (person: Human) => {
+    return `Hello ${person.name}, you ar ${person.age}, you are a ${person.gender}.`;
+}
+
+console.log(sayHi(person));
+
+export { };
